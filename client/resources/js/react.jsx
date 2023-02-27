cf7029235dc3eb0c894ead931ddb6501b287ddb1
srabonicse09 <126148229+srabonicse09@users.noreply.github.com>
2023-02-27 15:25:42 +0600

    Delete client directory

--- client/resources/js/react.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/index';
-
-import '../css/app.css'; 
-
-ReactDOM.render(<App />, document.getElementById('react-app'));
-
